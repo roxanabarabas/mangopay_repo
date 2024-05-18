4972be8f3e0819512699e7da25f5c9d0689e96a1
roxanabarabas <barabasroxana@gmail.com>
2024-05-18 11:47:32 +0300

    multiple env

--- global-setup.js
+++ global-setup.js
@@ -1,4 +1,31 @@
-import { FullConfig } from "@playwright/test";
+require('dotenv').config();
+
+const { chromium, expect } = require('@playwright/test');
+
+module.exports = async (config) => {
+  // set env variables
+  const {
+    storageState, importHTTPSErrors, headless, baseURL,
+  } = config.projects[0].use;
+
+  //instantiate
+  const browser = await chromium.launch({ headless });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto(baseURL, { timeout: 6000 });
+  await browser.close();
+}
+
+
+
+
+
+
+
+
+
+
+/*import { FullConfig } from "@playwright/test";
 import { config as _config } from 'dotenv';
 
 async function globalSetup(config) {
@@ -8,4 +35,4 @@ async function globalSetup(config) {
     });
   }
   
-  export default globalSetup;
+  export default globalSetup;*/
